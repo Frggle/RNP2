1a776133ac16c448e851e34275fe10a6ef4c28f8
Frggle <marc.kaepke@gmail.com>
2015-11-10 19:52:58 +0100

    JavaDoc hinzugefuegt und Hostname wird kontrolliert ob gueltig

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -8,7 +8,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.NoRouteToHostException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
@@ -16,55 +19,67 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
- * A simple Swing-based client for the chat server. Graphically it is a frame with a text field for entering messages
- * and a textarea to see the whole dialog. The client follows the Chat Protocol which is as follows. When the server
- * sends "SUBMITNAME" the client replies with the desired screen name. The server will keep sending "SUBMITNAME"
- * requests as long as the client submits screen names that are already in use. When the server sends a line beginning
- * with "NAMEACCEPTED" the client is now allowed to start sending the server arbitrary strings to be broadcast to all
- * chatters connected to the server. When the server sends a line beginning with "MESSAGE " then all characters
- * following this string should be displayed in its message area.
+ * ChatClient.java
+ * 
+ * Version 1.0
+ * Autor: Marc Kaepke & Anna Steinhauer
+ * Zweck: Ein TCP-Client verbindet sich, mit anderen Clients, mit einem Chat-Server.
+ * 		  Der Client folgt dabei dem Chat-Protokoll (SUBMITNAME, NAMEACCEPTED, MESSAGE und QUIT). 	
+ * 		  Der Client verfuegt ueber eine grafische Swing-Oberflaeche und zeigt den Chat-Verlauf an.
  */
 public class ChatClient {
 	
-	private BufferedReader in;
-	private PrintWriter out;
-	private JFrame frame = new JFrame("HAW - RN");
-	private JTextField textField = new JTextField(40);
-	private JTextArea messageArea = new JTextArea(8, 40);
+	private BufferedReader in;	// Eingabestream vom Server
+	private PrintWriter out;	// Ausgabestream zum Server
 	
-	private static ChatClient client;
-	private String nickname = "anonym";
-	private String hostname;
-	private final int PORT = 56789;
+	private JFrame frame = new JFrame("HAW - RN");	// Chat-Fenster
+	private JTextField textField = new JTextField(40);	// Eingabezeile 
+	private JTextArea messageArea = new JTextArea(8, 40);	// Ausgabebereich von allen Chat-Mitgliedern
+	
+	private String nickname = "anonym";	// Initial Nickname
+	
+	private String hostname;	// Hostname
+	private final int PORT = 56789;	// Portnummer, als Konstante
 	
 	/**
-	 * Constructs the client by laying out the GUI and registering a listener with the textfield so that pressing Return
-	 * in the listener sends the textfield contents to the server. Note however that the textfield is initially NOT
-	 * editable, and only becomes editable AFTER the client receives the NAMEACCEPTED message from the server.
+	 * Konstruktor
 	 */
 	public ChatClient() {
-		
+		builtGUI();
+	}
+	
+	/**
+	 * Erzeugt die GUI eines Clients und setzt Listeners.
+	 */
+	private void builtGUI() {
 		// Layout GUI
-		textField.setEditable(false);
-		messageArea.setEditable(false);
+		textField.setEditable(false);	// wird erst nach erfolgreichem Anmelden am Chat-Server aktiviert
+		messageArea.setEditable(false);	// Nachrichten-Bereich kann nicht veraendert werden
 		frame.getContentPane().add(textField, "North");
 		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
 		frame.pack();
 		
-		// 
+		// Listener auf das Beenden/ Schliessen des Fensters 
 		frame.addWindowListener(new WindowAdapter() {
-			
+			/**
+			 * Sobald das Fenster geschlossen wird, ohne dass der Benutzer sich abgemeldet hat,
+			 * wird der Chat-Server ueber den Befehl "/quit" informiert, dass der Benutzer den Chat verlassen hat.
+			 */
 			@Override
 			public void windowClosing(WindowEvent e) {
 				out.println("/QUIT");
 			}
+			@Override
+			public void windowClosed(WindowEvent e) {
+				out.println("/QUIT");
+			}
 		});
-		
-		// Add Listeners
+				
+		// Listener auf der Eingabezeile
 		textField.addActionListener(new ActionListener() {
 			/**
-			 * Responds to pressing the enter key in the textfield by sending the contents of the text field to the
-			 * server. Then clear the text area in preparation for the next message.
+			 * Sobald die ENTER-Taste gedrueckt wurde, wird der Inhalt an den Chat-Server geschickt.
+			 * Anschließend wird die Eingabezeile entleert/ geloescht
 			 */
 			public void actionPerformed(ActionEvent e) {
 				out.println(textField.getText());
@@ -74,77 +89,116 @@ public class ChatClient {
 	}
 	
 	/**
-	 * Prompt for and return the address of the server.
+	 * Erzeugt ein InputDialog und fordert zur Eingabe des Hostnames auf.
+	 * 
+	 * @return Hostname des zu verbindenen Servers
 	 */
 	private String getServerAddress() {
-		// TODO
 		do {
-			hostname = JOptionPane.showInputDialog(frame, "Enter IP Address of the Server:", "Welcome to RNP",
-					JOptionPane.QUESTION_MESSAGE);
-		} while(hostname.isEmpty());
+			hostname = JOptionPane.showInputDialog(frame, "IP Adresse vom Chat-Server:", "RNP2", JOptionPane.QUESTION_MESSAGE);
+		} while(hostname.isEmpty());	// wiederholt bis Hostname gefuellt ist
 		return hostname;
 	}
 	
 	/**
-	 * Prompt for and return the desired screen name.
+	 * Erzeugt ein InputDialog und fordert zur Eingabe des Nicknames auf.
+	 * 
+	 * @return Nickname des Benutzers
 	 */
 	private String getName() {
 		do {
-			nickname = JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
-					JOptionPane.PLAIN_MESSAGE);
-		} while(nickname.isEmpty());
+			nickname = JOptionPane.showInputDialog(frame, "Nickname angeben:", "Nickname", JOptionPane.PLAIN_MESSAGE);
+		} while(nickname.isEmpty());	// wiederholt bis Nickname gefuellt ist
 		return nickname;
 	}
-		
+	
 	/**
-	 * Connects to the server then enters the processing loop.
+	 * Verbindet sich mit dem Chat-Server und tritt der Prozess-Schleife bei.
 	 */
-	private void run() throws IOException {
-				
-		// Make connection and initialize streams
-		String serverAddress = getServerAddress();
-		Socket socket = new Socket(serverAddress, PORT);
+	private void startClient() throws IOException {
+		
+		/**
+		 * Erzeugt den TCP-Socket und verbindet sich.
+		 * Wenn Hostname ungueltig/ nicht erreichbar, dann wiederhole Eingabe.
+		 */
+		String serverAddress = null;
+		Socket socket = null;
+		boolean isConnected = false;
+		do {
+			try {
+				serverAddress = getServerAddress();
+				socket = new Socket(serverAddress, PORT);	// Verbindungsaufbau zum Server
+				isConnected = true;
+			} catch(NoRouteToHostException e) {
+				System.err.println(e);
+				isConnected = false;
+			} catch(UnknownHostException e2) {
+				System.err.println(e2);
+				isConnected = false;
+			} catch(ConnectException e3) {
+				System.err.println(e3);
+				isConnected = false;
+			}
+		} while(!isConnected);
+
+		// Initialisiere In- und Out-Stream
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		out = new PrintWriter(socket.getOutputStream(), true);
 		
-		// TODO Check if ServerSocket is available 
-		
+		// Client beenden?
 		boolean serviceRequested = true;
 		
-		// Process all messages from server, according to the protocol.
+		/**
+		 * Durchlaeuft das Protokoll bis Client beendet wird.
+		 * 
+		 * SUBMITNAME -> Aufforderung fuer Nickname
+		 * NAMEACCEPTED -> Nickname wird beim Server registriert
+		 * MESSAGE -> Es folgt eine Chat-Nachricht vom Server
+		 * QUIT -> Beendet den Client und meldet ihn ab
+		 */
+		// Anhand der Befehle "
 		while(serviceRequested) {
 			try {
-				String line = in.readLine();
+				String line = in.readLine();	// Antwort vom Server
+				
+				// Aufforderung fuer Angabe des Nicknames; serverseitig
 				if(line.startsWith("SUBMITNAME")) {
 					out.println(getName());
-					frame.setTitle(frame.getTitle() + " as " + nickname);
+					frame.setTitle(frame.getTitle() + " as " + nickname);	// passte Titel vom Client Fenster an
+					
+				// Nickname wurde vom Server akzeptiert -> Eingabefeld wird aktiviert
 				} else if(line.startsWith("NAMEACCEPTED")) {
 					textField.setEditable(true);
+				
+				// Server schickt Chat-Nachricht an Client zum Anzeigen im Ausgabebereich 
 				} else if(line.startsWith("MESSAGE")) {
 					messageArea.append(line.substring(8) + "\n");
+					
+				// Server schickt Bestaetigung; Client erfolgreich abgemeldet
 				} else if(line.startsWith("QUIT")) {
 					serviceRequested = false;
 					JOptionPane.showMessageDialog(frame, "You left the conversation.", "Good bye", JOptionPane.OK_OPTION);
 					frame.dispose();
-				} 
+				}
+				
+			// Fehlermeldung, wenn der Chat-Server nicht mehr erreichbar ist
 			} catch(NullPointerException e) {
 				serviceRequested = false;
-				JOptionPane.showMessageDialog(frame, "The Chat-Server is down.", "Attention", JOptionPane.OK_OPTION);
+				JOptionPane.showMessageDialog(frame, "Der Chat-Server ist nicht mehr erreichbar.", "Achtung", JOptionPane.OK_OPTION);
 				frame.dispose();
 			}
 		}
-		socket.close();
+		socket.close(); // Verbindungsabbau
 	}
 	
 	/**
-	 * Runs the client as an application with a closeable frame.
+	 * Startet einen Client 
 	 */
 	public static void main(String[] args) throws Exception {
-		//        ChatClient client = new ChatClient();
-		client = new ChatClient();
+		ChatClient client = new ChatClient();
 		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		client.frame.setLocationRelativeTo(null);
-		client.frame.setVisible(true);
-		client.run();
+		client.frame.setLocationRelativeTo(null); // positioniert das Fenster mittig
+		client.frame.setVisible(true);	// macht es sichtbar
+		client.startClient();	// startet den Client
 	}
 }
