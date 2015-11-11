1e1e310fa788ffa12ee2632dbe9a1e6c7a75a287
Frggle <marc.kaepke@gmail.com>
2015-11-11 12:40:32 +0100

    JavaDoc ergänzt

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -11,131 +11,113 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
- * A multithreaded chat room server. When a client connects the server requests
- * a screen name by sending the client the text "SUBMITNAME", and keeps
- * requesting a name until a unique one is received. After a client submits a
- * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
- * from that client will be broadcast to all other clients that have submitted a
- * unique screen name. The broadcast messages are prefixed with "MESSAGE ".
+ * ChatServer.java
+ * 
+ * Version: 1.0
+ * Autor: Marc Kaepke & Anna Steinhauer
+ * Zweck: Ein multi-thread/client Chat Raum Server (TCP Server-Socket).
  */
 public class ChatServer {
 
-    /**
-     * The port that the server listens on.
-     */
-    private static final int PORT = 56789;
+    private static final int PORT = 56789; // Port, als Konstante
+
+    private static List<String> users = new ArrayList<String>();	// Liste alles Mitglieder im Chat-Raum
+    private static List<PrintWriter> writers = new ArrayList<PrintWriter>();	// Liste aller PrintWriters (out to client)
+    private static List<String> session = new ArrayList<String>();	// Liste aller gesendeten Nachrichten; serverseitig
+
+    private static JFrame logFrame;	// Server-Log Fenster
+    private static JTextArea logTextArea;	// Anzeigebereich des Logs
 
     /**
-     * The set of all names of clients in the chat room. Maintained so that we
-     * can check that new clients are not registering name already in use.
-     */
-    private static Set<String> users = new HashSet<String>();
-
-    /**
-     * The set of all the print writers for all the clients. This set is kept so
-     * we can easily broadcast messages.
-     */
-    private static Set<PrintWriter> writers = new HashSet<PrintWriter>();
-
-    /**
-     * TODO
-     */
-    private static List<String> session = new ArrayList<String>();
-
-    private static JFrame frame;
-    private static JTextArea textArea;
-
-    /**
-     * The appplication main method, which just listens on a port and spawns
-     * handler threads.
+     * main Methode; die ein Server-Log-Fenster erstellt, einen Port beobachtet und neue Handler-Threads startet
      */
     public static void main(String[] args) throws Exception {
-        System.out.print("The chat server is running");
-        ServerSocket listener = new ServerSocket(PORT);
-        System.out.println(" on IP: "
-                + InetAddress.getLocalHost().getHostAddress());
+        System.out.print("Chat Server wurde gestartet");
 
-        frame = new JFrame("Chat Server - Log");
-        textArea = new JTextArea(20, 40);
-        textArea.setEditable(false);
-        frame.getContentPane().add(new JScrollPane(textArea), "Center");
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        ServerSocket listener = new ServerSocket(PORT);	// TCP Server Socket
 
+        // Layout GUI
+        logFrame = new JFrame("Chat Server - Log");
+        logTextArea = new JTextArea(20, 40);
+        logTextArea.setEditable(false);
+        logFrame.getContentPane().add(new JScrollPane(logTextArea), "Center");
+        logFrame.pack();
+        logFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        logFrame.setVisible(true);
+
+        // Basis Infos werden dem Log beigefuegt
         DateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd");
         Calendar cal = Calendar.getInstance();
-        textArea.append("The Chat Server ist running");
-        textArea.append(" on IP: "
-                + InetAddress.getLocalHost().getHostAddress() + "\n");
-        textArea.append("---" + dateformat.format(cal.getTime()) + "---\n");
-        textArea.append("\n");
+        logTextArea.append("Der Chat Server laeuft unter der IP: ");
+        logTextArea.append(InetAddress.getLocalHost().getHostAddress() + "\n");
+        logTextArea.append("---" + dateformat.format(cal.getTime()) + "---\n\n");
 
-        Socket connectionSocket;
+        /**
+         * Endlosschleife und nimmt jede eingehende Verbindungsanfrage (von Clients) an.
+         * Startet fuer jede Verbindung einen Thread ("Handler").
+         */
+        Socket connectionSocket;	// TCP-Standard-Socket
         try {
             while (true) {
-                connectionSocket = listener.accept();
-                new Handler(connectionSocket).start();
-                // new Handler(listener.accept()).start();
+                connectionSocket = listener.accept();	// "Hand-Shake"
+                new Handler(connectionSocket).start();	// Handler-Thread mit Socket
             }
         } finally {
-            listener.close();
+            listener.close();	// Verbindungsabbau
         }
     }
 
     /**
-     * A handler thread class. Handlers are spawned from the listening loop and
-     * are responsible for a dealing with a single client and broadcasting its
-     * messages.
+     * Jeder Client besitzt seinen eigenen Handler-Thread; 
+     * verwaltet die Verbindung (Socket) zum Chat-Server und uebertraegt die Nachrichten.  
      */
     private static class Handler extends Thread {
-        private String name;
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
+        private String name;	// Client Benutzername
+        private Socket socket;	// TCP-Standard-Socket
+        private BufferedReader in;	// Eingangsstream vom Client
+        private PrintWriter out;	// Ausgangsstream zum Client
 
         /**
-         * Constructs a handler thread, squirreling away the socket. All the
-         * interesting work is done in the run method.
+         * Konstruktor
          */
         public Handler(Socket socket) {
             this.socket = socket;
         }
 
         /**
-         * Services this thread's client by repeatedly requesting a screen name
-         * until a unique one has been submitted, then acknowledges the name and
-         * registers the output stream for the client in a global set, then
-         * repeatedly gets inputs and broadcasts them.
+         * Fordert zunaechst den Nickname an, bis dieser akzeptiert wird.
+         * Anschliessend wird das Protokoll in einer Dauerschleife durchlaufen und kommuniziert mit dem Client.
          */
         public void run() {
             try {
-
-                // Create character streams for the socket.
+            	// Erzeugt den In- und Out-Stream des Clients
                 in = new BufferedReader(new InputStreamReader(
                         socket.getInputStream()));
                 out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Request a name from this client. Keep requesting until
-                // a name is submitted that is not already used. Note that
-                // checking for the existence of a name and adding the name
-                // must be done while locking the set of names.
+                /**
+                 * Server fordert zur Eingabe des Nicknames auf.
+                 * Der Nickname darf im Chat-Raum noch nicht vergessen sein.
+                 */
                 while (true) {
-                    out.println("SUBMITNAME"); // Bestaetigung vom Server an
-                                               // Client
+                    out.println("SUBMITNAME"); 
+                    
                     name = in.readLine();
                     if (name == null) {
                         return;
                     }
                     writeServerLog("        ", name + " SUBMITNAME");
+
+                    /**
+                     *  Damit Threads sich nicht in die Quere kommen,
+                     *  ist die Abfrage ob der Nickname vergeben ist ein kritischer Abschnitt. 
+                     */
                     synchronized (users) {
                         if (!users.contains(name)) {
                             users.add(name);
@@ -144,27 +126,30 @@ public class ChatServer {
                     }
                 }
 
-                // Now that a successful name has been chosen, add the
-                // socket's print writer to the set of all writers so
-                // this client can receive broadcast messages.
+                // Der neue Benutzer wird "akzeptiert" -> Server schickt Bestaetigung an Client
                 out.println("NAMEACCEPTED");
                 writeServerLog("        ", name + " NAMEACCEPTED");
 
+                // Informiert die Chat-Mitgliedern, dass ein neuer Benutzer beigetreten ist
                 for(PrintWriter writer : writers) {
                 	writer.println("MESSAGE        " + name + " joined");
                 }
                 writeServerLog("        ", name + " joined");
-                writers.add(out);
+                
+                writers.add(out); // Fuegt den neuen WriterStream der Liste aller Mitglieder-Streams bei
 
                 Calendar cal = Calendar.getInstance();
                 SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
-                // Accept messages from this client and broadcast them.
-                // Ignore other clients that cannot be broadcasted to.
+                /**
+                 * Nimmt die Nachrichten des Clients entgegen und antwortet bzw. verteilt 
+                 * die Nachrichten entsprechend -> nur an Client selbst oder an alle Clients im Chat
+                 */
                 while (true) {
                     String input = in.readLine();
                     if (input == null) {
                         return;
+                    // Wenn der Client den Chat verlassen will
                     } else if (input.toUpperCase().startsWith("/QUIT")) {
                         out.println("QUIT");
                         writeServerLog("        ", name + " disconnected");
@@ -172,6 +157,7 @@ public class ChatServer {
                             writer.println("MESSAGE " + name + " ("
                                     + sdf.format(cal.getTime()) + ") disconnected");
                         }
+                    // Wenn der Client wissen will, welche Mitglieder im Chat-Raum sind
                     } else if (input.toUpperCase().startsWith("/USER")) {
                     	writeServerLog(name, input);
                         out.println("MESSAGE        list of users:");
@@ -180,43 +166,52 @@ public class ChatServer {
                                 out.println("MESSAGE        " + user);
                             }
                         }
+                    // Wenn der Client Hilfe anfordert, welche Befehle ihm zur Verfuegung stehen
                     } else if (input.toUpperCase().startsWith("/HELP")) {
                     	writeServerLog(name, input);
                         out.println("MESSAGE        /user => list of connected users.");
                         out.println("MESSAGE        /quit => disconnect from Chat-Server.");
+                        writeServerLog("", "        /user => list of connected users.");
+                        writeServerLog("", "        /quit => disconnect from Chat-Server.");
+                    // Wenn eine Nachricht an alle gehen soll
                     } else {
                         for (PrintWriter writer : writers) {
                             writeServerLog(name, input);
                             writer.println("MESSAGE " + name + " ("
                                     + sdf.format(cal.getTime()) + ") : " + input);
                         }
-                        
                     }
                 }
             } catch (IOException e) {
                 System.out.println(e);
             } finally {
-                // This client is going down! Remove its name and its print
-                // writer from the sets, and close its socket.
+            	// Wenn der Client down ist ...
                 if (name != null) {
-                    users.remove(name);
+                    users.remove(name);		// entferne den Benutzer aus der Chat-Mitglieder-Liste
                 }
                 if (out != null) {
-                    writers.remove(out);
+                    writers.remove(out);	// entferne PrintWriter vom abgemeldeten Benutzer
                 }
                 try {
-                    socket.close();
+                    socket.close();	// Verbindungsabbau
                 } catch (IOException e) {
                 }
             }
         }
         
+        /**
+         * Schreibt die Nachricht in eine Session-ArrayList und zeigt sie anschliessend im Server-Log-Fenster an.
+         * 
+         * <beforeDate> (hh:mm) : <afterDate>
+         * @param beforeDate, String wird vor der Uhrzeit eingefuegt 
+         * @param afterDate, String wird nach der Uhrzeit eingefuegt
+         */
         private void writeServerLog(String beforeDate, String afterDate) {
         	Calendar cal = Calendar.getInstance();
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         	
         	session.add(beforeDate + " (" + sdf.format(cal.getTime()) + ") : " + afterDate);
-        	textArea.append(session.get(session.size() - 1) + "\n");
+        	logTextArea.append(session.get(session.size() - 1) + "\n");
         }
     }
 }
